876c4500677d5a933a3e22a06fd9bceb49e8cce3
88bones <123712642+88bones@users.noreply.github.com>
2025-09-02 19:38:04 +0545

    env changes

--- client/src/services/getRoast.js
+++ client/src/services/getRoast.js
@@ -2,9 +2,11 @@ import axios from "axios";
 
 export const postTopArtist = async (topArtists) => {
   try {
-    const res = await axios.post(`http://127.0.0.1:3001/api/roaster/artist`, {
-      musicData: topArtists,
-    });
+    const res = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/roaster/artist`,
+      { musicData: topArtists }
+    );
+
     return res.data;
   } catch (err) {
     if (err.response && err.response.data && err.response.data.error) {
